8f8b603c6433ec7eef34fbc33448fc8528aec019
Yoonjae Choi <aeioulofi@gmail.com>
2023-08-17 21:15:05 -0500

    Print hello world in console

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    // comment
+
+    /*
+        comment
+    */
+    
+    std::cout << "I like noodles!" << '\n';
+    std::cout << "It tastes delicious!" << std::endl;
+    return 0;
+}
