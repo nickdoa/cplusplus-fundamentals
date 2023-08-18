60254e19f17b68191c531a272c8f475e6637fc1b
Yoonjae Choi <aeioulofi@gmail.com>
2023-08-17 21:48:45 -0500

    Functions returning strings in C++

---
+++ StringReturn.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+std::string concatStrings(std::string string1, std::string string2); 
+
+int main() {
+
+    std::string firstName = "Nick";
+    std::string lastName = "C";
+    std::string fullName = concatStrings(firstName, lastName);
+
+    std::cout << "Hello " << fullName;
+
+    return 0;
+}
+
+std::string concatStrings(std::string string1, std::string string2) {
+    return string1 + " " + string2;
+}
