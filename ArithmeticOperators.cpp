33362fb7f0bc265a150411829266ac8d5105d4af
Yoonjae Choi <aeioulofi@gmail.com>
2023-08-17 21:23:15 -0500

    Basic C++ arithmetic operators

---
+++ ArithmeticOperators.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main () {
+
+    int students = 7;
+
+    //students = students + 2;
+    //students+=1; 
+    //students++;   in increments
+
+    //students = students - 1;
+    //students-=2;
+    //students--;   in increments
+
+    //students = students * 2;
+    //students*=2;
+    
+    //students = students / 2;
+    //students/=2;
+
+    int remainder = students % 2; //gives remainder of any devision
+
+    std::cout << remainder;
+
+    return 0;
+}
